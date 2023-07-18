4e310805828eb5142c4f352d8232de37f7cead52
SirDon3 <110694491+SirDon3@users.noreply.github.com>
2023-07-18 18:56:40 +0300

    unicafe step6

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button> 
 
 const StatisticLine = ({ text, ratingValue}) => <p>{text} : {ratingValue} </p>
 
@@ -13,11 +13,11 @@ const Average = ({total, avg }) => <p>Average : {total / avg}</p>
 
 const Percentage = ({part, whole }) => <p>Percentage : {part / whole * 100}%</p>
 
-const Statistics = (props) => {
+const Statistics = (props) => { // The Statistics component is a large component that calls other smaller componets to retrive statictics information 
 
 
 
-  if (props.totalAll === 0) {
+  if (props.totalAll == 0) { // The if statement ensure that message is printed if there are no feedback given
     return (
       <div>
         <p>No feedback given</p>
@@ -27,24 +27,48 @@ const Statistics = (props) => {
 
   const avg = 3
 
-  return(
+  return( // If there are 1 or more feedback given the Statistics component returns a set of other smaller componets to retrive statictics information
+
     <div>
 
         <Heading text = "Statistics"></Heading>
 
-        <StatisticLine ratingValue={props.contentGoodrv} text={props.contentGoodtxt}></StatisticLine>
-        <StatisticLine ratingValue={props.contentNeutralrv} text={props.contentNeutraltxt}></StatisticLine>
-        <StatisticLine ratingValue={props.contentBadrv} text={props.contentBadtxt}></StatisticLine>
-        <Total all={props.totalAll}></Total>
-        <Average total={props.avarageAll} avg={avg}></Average>
-        <Percentage part={props.percentagePart} whole={props.percentagewhole}></Percentage>
-  
+      <table>
+        <tbody>
+
+        <tr>
+               <td><StatisticLine ratingValue={props.contentGoodrv} text={props.contentGoodtxt}></StatisticLine></td>
+       </tr>
+
+        <tr>
+               <td><StatisticLine ratingValue={props.contentNeutralrv} text={props.contentNeutraltxt}></StatisticLine></td>
+        </tr>
+
+         <tr>
+               <td><StatisticLine ratingValue={props.contentBadrv} text={props.contentBadtxt}></StatisticLine></td>  
+        </tr>
+
+         <tr>
+               <td><Total all={props.totalAll}></Total></td>
+        </tr>
+
+         <tr>
+               <td> <Average total={props.avarageAll} avg={avg}></Average></td>
+        </tr>
+
+         <tr>
+               <td><Percentage part={props.percentagePart} whole={props.percentagewhole}></Percentage></td>
+        </tr>
+
+        </tbody>
+      </table>
+
    </div>
   )
 }
 
-const ButtonFeedback = (props) => {
-  console.log(props)
+const ButtonFeedback = (props) => { // The ButtonFeedback is a component that calls a reusable Button component to creates three buttons 
+ 
 
   return(
     <div>
@@ -52,14 +76,14 @@ const ButtonFeedback = (props) => {
          <Button handleClick={props.handleGoodClickEvent} text = {props.Goodtxt}></Button>
          <Button handleClick={props.handleNeutralClickEvent} text = {props.Neutraltxt}></Button>
          <Button handleClick={props.handleBadClickEvent} text = {props.Badtxt}></Button>
-         
+
     </div>
 
   )
 }
 
-const App = () => {
-  // save clicks of each button to its own state
+const App = () => { // App is the main component that brings everthing together 
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -69,25 +93,29 @@ const App = () => {
   const Bad = "Bad"
 
  
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)  
-  const all = good + neutral + bad 
-  const avg = 3
-  const n = 5
-  const n1 = 10
+  const HandleGoodClick = () => setGood(good + 1)
+  const HandleNeutralClick = () => setNeutral(neutral + 1)
+  const HandleBadClick = () => setBad(bad + 1)  
+  // State Hook and handle functions are used to keep track of and mesure clicks 
+
+  const All = good + neutral + bad // Calculation for all feedback
 
 
-  return (
+
+  return ( /* The App compoent returns a small heading component to dispay a text and 
+               the ButtonFeedback and Statistics component which used the infromation stored 
+               from the State hook and handle function to display information and perform opertaion
+               based on actions */
     <div>
+
       <Heading text = "Give Feedback"></Heading>
 
 
       <ButtonFeedback 
       
-            handleGoodClickEvent={handleGoodClick} Goodtxt={Good}
-            handleNeutralClickEvent={handleNeutralClick} Neutraltxt={Neutral}
-            handleBadClickEvent={handleBadClick} Badtxt={Bad}
+            handleGoodClickEvent={HandleGoodClick} Goodtxt={Good}
+            handleNeutralClickEvent={HandleNeutralClick} Neutraltxt={Neutral}
+            handleBadClickEvent={HandleBadClick} Badtxt={Bad}
       
       
       ></ButtonFeedback>
@@ -96,15 +124,11 @@ const App = () => {
                   contentGoodrv={good} contentGoodtxt={Good}
                   contentNeutralrv={neutral} contentNeutraltxt={Neutral}
                   contentBadrv={bad} contentBadtxt={Bad}
-                  totalAll={all}
-                  avarageAll={all} 
-                  percentagePart={good} percentagewhole={all}
+                  totalAll={All}
+                  avarageAll={All} 
+                  percentagePart={good} percentagewhole={All}
 
-      > 
-      </Statistics>
-      
-
-
+      ></Statistics>
       
     </div>
   )
